f6f99908ca696ece01f225f33728579cce71b312
Aalums <59070194@it.kmitl.ac.th>
2019-03-11 22:37:38 +0700

    เพิ่ม MenuController
    ยังไม่ทำ GET/Menu?search=:name

---
+++ src/main/java/com/demo/Recipe/Menu/MenuController.java
@@ -0,0 +1,36 @@
+package com.demo.Recipe.Menu;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MenuController {
+
+    @Autowired
+    private MenuService menuService;
+
+    @GetMapping("/Category")
+    public Menu getCategory(){
+        return menuService.getCategory();
+    }
+
+    @GetMapping("/Category/{name}/Menu")
+    public Menu getMenu(@PathVariable String name){
+        return menuService.getMenu(name);
+    }
+
+    @GetMapping("/Member/{id}/Menu")
+    public Menu getMenu(@PathVariable String memberId, String name){
+        return menuService.getMenu(memberId, name);
+    }
+
+    @GetMapping("/Menu/{id}/MenuDetail")
+    public Menu getMenuDetail(@PathVariable String menuId){
+        return menuService.getMenuDetail(menuId);
+    }
+
+//    GET/Menu?search=:name
+
+}
