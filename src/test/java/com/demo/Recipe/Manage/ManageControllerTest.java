7c795ccf02b02816a92542ff10f44632119671e5
preawphuak <59070126@it.kmitl.ac.th>
2019-03-12 13:23:07 +0700

    Add ManageControllerTest

---
+++ src/test/java/com/demo/Recipe/Manage/ManageControllerTest.java
@@ -0,0 +1,30 @@
+package com.demo.Recipe.Manage;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+import static org.junit.Assert.*;
+
+public class ManageControllerTest{
+
+        @Autowired
+        private TestRestTemplate restTemplate;
+
+        public void createMenu(){
+                ManageService manageService = restTemplate.getForObject("/Member/eyenach/Menu" ,ManageService.class);
+                assertEquals("eyenach" ,manageService.getManage().getMemberId());
+                }
+
+        @Test
+        public void updateMenu(){
+                ManageService manageService = restTemplate.getForObject("/Menu/f0000001/MenuDetail" ,ManageService.class);
+                assertEquals("f0000001" ,manageService.getManage().getMenuId());
+                }
+        @Test
+        public void deleteMenu(){
+                ManageService manageService = restTemplate.getForObject("/Menu/f0000001" ,ManageService.class);
+                assertEquals("f0000001" ,manageService.getManage().getMenuId());
+                }
+
+        }
