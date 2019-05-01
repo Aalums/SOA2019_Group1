629c7f4b7227a148645323feda64bf2276423633
preawphuak <59070126@it.kmitl.ac.th>
2019-05-01 22:01:00 +0700

    Edit Endpoint (MenuService)
    จากตัวใหญ่เป็นตัวเล็ก

--- service/MenuService/src/main/java/com/example/MenuService/Controller/MenuController.java
+++ service/MenuService/src/main/java/com/example/MenuService/Controller/MenuController.java
@@ -13,31 +13,31 @@ public class MenuController {
     private MenuService menuService;
 
     //Get All Category
-    @GetMapping("/Category")
+    @GetMapping("/category")
     public MenuService getCategory(){
         return new MenuService();
     }
 
     //Get Menu in Category
-    @GetMapping("/Category/{type}/Menu")
+    @GetMapping("/category/{type}/menu")
     public Menu getMenu(@PathVariable String type){
         return menuService.getMenuCategory(type);
     }
 
     //Get Menu of User
-    @GetMapping("/Member/{memberId}/Menu")
+    @GetMapping("/member/{memberId}/menu")
     public Menu getMenuByMember(@PathVariable String memberId){
         return menuService.getMenuByMember(memberId);
     }
 
     //Get MenuDetail of menuId
-    @GetMapping("/Menu/{menuId}/MenuDetail")
+    @GetMapping("/menu/{menuId}/menudetail")
     public Menu getMenuDetail(@PathVariable String menuId){
         return menuService.getMenuDetail(menuId);
     }
 
     //Get Menu By search
-    @GetMapping("/Menu/search/{menuName}")
+    @GetMapping("/menu/search/{menuName}")
     public Menu getMenuByMenuName(@PathVariable String menuName){
         return menuService.getMenu(menuName);
     }
