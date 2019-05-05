1b176e0fd85a175a2444ddeda1978040a0d5b23d
Aalums <59070194@it.kmitl.ac.th>
2019-05-05 16:42:53 +0700

    เพิ่มโค้ด Register Manage-service
    ยังลองรันไม่ได้ เพราะ Test error

--- service/manage_service/src/main/java/com/recipe/service/manage/ManageApplication.java
+++ service/manage_service/src/main/java/com/recipe/service/manage/ManageApplication.java
@@ -2,8 +2,10 @@ package com.recipe.service.manage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ManageApplication {
 
     public static void main(String[] args) {
